b9be7ac4e739c0f2fcb5af52c28707b4f2cf4811
wak1lon <wakilontree12000@gmail.com>
2026-08-18 09:52:59 -0500

    Adiciona configuração Firebase

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,13 @@
+// A configuração web do Firebase não é um segredo. A segurança real fica nas regras do Firestore.
+// Preencha os dados do seu projeto e altere enabled para true.
+export const FIREBASE_CONFIG = {
+  enabled: false,
+  config: {
+    apiKey: 'SUBSTITUA_AQUI',
+    authDomain: 'SEU-PROJETO.firebaseapp.com',
+    projectId: 'SEU-PROJETO',
+    storageBucket: 'SEU-PROJETO.appspot.com',
+    messagingSenderId: 'SUBSTITUA_AQUI',
+    appId: 'SUBSTITUA_AQUI'
+  }
+};
